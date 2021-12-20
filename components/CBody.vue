f9cbdf9c71957f8963bdfcb09f9c7cbe7b87e3d2
Mourathiago <moura.francisco.thiago@hotmail.com>
2021-12-20 12:55:17 -0400

    Add filter in table

--- components/CBody.vue
+++ components/CBody.vue
@@ -11,19 +11,17 @@
                         </b-col>
                         <b-col class="mt-3" cols="12">
                             <b-row>
-                                <b-col cols="3">
-                                    <b-form-select v-model="selected" :options="options"></b-form-select>
+                                <b-col cols="4">
+                                    <label for="nationalities">Nacionalidades</label>
+                                    <b-form-select id="nationalities" v-model="selected" :options="options" multiple :select-size="2" @change="selectNat"></b-form-select>
                                 </b-col>
-                                <b-col cols="8">
-                                    <b-form-input v-model="nome" placeholder="Insira o nome ou a nacionalidade" @keyup.enter="searchList"></b-form-input>
-                                </b-col>
-                                <b-col cols="1">
-                                    <b-button variant="primary" @click="searchList"><b-icon-search></b-icon-search></b-button>
+                                <b-col class="mt-5" cols="8">
+                                    <b-form-input v-model="nome" placeholder="Busca por nome" @keyup.enter="searchList"></b-form-input>
                                 </b-col>
                             </b-row>
                         </b-col>
                         <b-col class="mt-2" cols="12">
-                            <CTable />
+                            <CTable :filter="nome" />
                         </b-col>
                     </b-row>
                 </b-col>
@@ -33,7 +31,7 @@
 </template>
 
 <script>
-import { BContainer, BRow, BCol, BFormSelect, BFormInput, BButton, BIconSearch } from 'bootstrap-vue'
+import { BContainer, BRow, BCol, BFormSelect, BFormInput } from 'bootstrap-vue'
 
 export default {
     name: 'CBody',
@@ -43,23 +41,39 @@ export default {
         "b-col": BCol,
         "b-form-select": BFormSelect,
         "b-form-input": BFormInput,
-        "b-button": BButton,
-        "b-icon-search": BIconSearch
     },
     data() {
       return {
-        selected: 'name',
+        selected: ['AU', 'BR', 'CA', 'CH', 'DE', 'DK', 'ES', 'FI', 'FR', 'GB', 'IE', 'IR', 'NO', 'NL', 'NZ', 'TR', 'US'],
         nome: "",
         options: [
-          { value: 'name', text: 'Nome' },
-          { value: 'nat', text: 'Nacionalidade' }
+            { value: 'AU', text: 'Australia' },
+            { value: 'BR', text: 'Brasil' },
+            { value: 'CA', text: 'Brasil' },
+            { value: 'CH', text: 'Brasil' },
+            { value: 'DE', text: 'Brasil' },
+            { value: 'DK', text: 'Brasil' },
+            { value: 'ES', text: 'Brasil' },
+            { value: 'FI', text: 'Brasil' },
+            { value: 'FR', text: 'Brasil' },
+            { value: 'GB', text: 'Brasil' },
+            { value: 'IE', text: 'Brasil' },
+            { value: 'IR', text: 'Brasil' },
+            { value: 'NO', text: 'Brasil' },
+            { value: 'NL', text: 'Brasil' },
+            { value: 'NZ', text: 'Brasil' },
+            { value: 'TR', text: 'Brasil' },
+            { value: 'US', text: 'Estados Unidos' }
         ]
       }
     },
+    mounted() {
+        this.$store.commit('open')
+    },
     methods: {
-        async searchList() {
-            if(this.nome !== "" && this.selected === "nat") {
-                const result = await this.$axios.get("https://randomuser.me/api/?seed=94c025a0a1ca3503&page=1&results=10&nat="+this.nome)
+        async selectNat() {
+            if(this.selected.length !== 0) {
+                const result = await this.$axios.get("https://randomuser.me/api/?seed=94c025a0a1ca3503&page=1&results=50&nat="+this.selected)
                 this.$store.commit('user/add', result.data.results)
             }
         }
--- components/CTable.vue
+++ components/CTable.vue
@@ -3,11 +3,29 @@
         <p v-if="$fetchState.pending"><b-icon-three-dots animation="cylon" font-scale="4"></b-icon-three-dots></p>
         <p v-else-if="$fetchState.error">An error occurred :(</p>
         <div v-else>
-            <b-table class="text-center" :fields="fields" :items="items" bordered>
+            <b-table id="dataTable" class="text-center" :filter="filter" :fields="fields" :items="items" :per-page="perPage" :current-page="currentPage" bordered @filtered="onFiltered">
                 <template #cell(action)="data">
                     <NuxtLink :to="{ path: 'user', query: { token: data.item.login.uuid }}"><b-button variant="primary" @click="opened">Ver mais</b-button></NuxtLink>
                 </template>
             </b-table>
+            <b-pagination
+                v-model="currentPage"
+                pills
+                :total-rows="rows"
+                :per-page="perPage"
+                aria-controls="dataTable"
+                class="mx-auto"
+                align="center"
+            >
+                <template #first-text><span>&#60;&#60;</span></template>
+                <template #prev-text><span>&#60;</span></template>
+                <template #next-text><span>&#62;</span></template>
+                <template #last-text><span>&#62;&#62;</span></template>
+                <template #page="{ page, active }">
+                    <b v-if="active">{{ page }}</b>
+                    <i v-else>{{ page }}</i>
+                </template>
+            </b-pagination>
         </div>
     </div>
 </template>
@@ -18,6 +36,18 @@ import { mapGetters } from 'vuex'
 export default {
     name: 'CTable',
     components: { "b-table": BTable, "b-button": BButton, "b-icon-three-dots": BIconThreeDots },
+    props: {
+        filter: {
+            Type: String,
+            required: true,
+            validator: (value) => {
+                return (
+                value !== null ||
+                value.match('/([^1-9])w+/g') > 1
+                )
+            }
+        }
+    },
     data() {
         return {
             fields: [
@@ -25,16 +55,21 @@ export default {
                 { key: "gender", label: "Gênero", sortable: true },
                 { key: "dob", label: "Aniversário", formatter: 'birth' },
                 { key: "action", label: "Ação" }
-            ]
+            ],
+            perPage: 10,
+            currentPage: 1
         }
     },
     async fetch() {
-        const result = await this.$axios.get("https://randomuser.me/api/?seed=94c025a0a1ca3503&page=1&results=10&noinfo")
+        const result = await this.$axios.get("https://randomuser.me/api/?seed=94c025a0a1ca3503&page=1&results=50&noinfo")
         this.$store.commit('user/add', result.data.results)   
     },
-    computed: mapGetters({
-        items: 'user/users'
-    }),
+    computed: {
+        ...mapGetters({items: 'user/users'}),
+        rows() {
+            return this.items.length
+        }
+    },
     methods: {
         opened() {
             this.$store.commit('open')
@@ -45,6 +80,10 @@ export default {
         birth(value) {
             const split = value.date.split('T')[0].split('-')
             return `${split[2]}/${split[1]}/${split[0]}`
+        },
+        onFiltered(filteredItems) {
+            this.rows = filteredItems.length
+            this.currentPage = 1
         }
     }
 }
